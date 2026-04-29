2769a9be229402e53f431b9291c587fca6e00b09
Kevin <kjfox001@gmail.com>
2026-04-29 14:42:44 -0400

    Added titles and description too the shop

--- group_project_master/src/pages/Shop.jsx
+++ group_project_master/src/pages/Shop.jsx
@@ -2,6 +2,7 @@
 import MyFooter from '../components/layout/Footer';
 import MyNav from '../components/layout/navbar';
 import ItemCards from '../components/layout/ItemCards.jsx';
+import '../assets/titles.css';
 
 function Shop() {
   return (
@@ -12,9 +13,10 @@ function Shop() {
       
       <main>
         {/* we are going to add some content here later */}
+        <h1 class = 'title'>Shop</h1>
+        <h3 class = 'description'>These deals won't cost you an arm or a leg!</h3>
         <ItemCards />
       </main>
-
       
     </div>
   );
